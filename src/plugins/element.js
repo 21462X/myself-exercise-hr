7b606144353cc3a0e8e21fdeaf654c3d92043ea6
21462X <2274457767@qq.com>
2020-04-26 00:19:53 +0800

    主页头界面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,14 +2,24 @@ import Vue from 'vue'
 
 // 导入element-ui 样式
 import 'element-ui/lib/theme-chalk/index.css'
-import { Button ,Form,FormItem,Input,Checkbox,Message} from 'element-ui'
+import { Button ,Form,FormItem,Input,Checkbox,Message,
+    Container, Header, Aside, Main,
+    Dropdown, DropdownMenu, DropdownItem, MessageBox,} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Checkbox)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
 
 // 把Messag挂载到Vue的原型对象上
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
 
