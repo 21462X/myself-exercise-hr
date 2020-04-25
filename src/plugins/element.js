f491e7d4153b9a9f9b571d14b0671b3f8c6abf10
21462X <2274457767@qq.com>
2020-04-25 20:45:57 +0800

    修改login登录按钮的事件处理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import { Button ,Form,FormItem,Input,Message} from 'element-ui'
+// 导入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button ,Form,FormItem,Input,Checkbox,Message} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-Vue.use(Message)
+Vue.use(Checkbox)
+
+//把Message挂载到Vue的原型对象上
+Vue.prototype.$message = Message
 
 
